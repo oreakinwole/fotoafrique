041dd468b13bf87674bf75a70647e76be5e13e1c
oreakinwole <oreakinwole@gmail.com>
2024-08-19 19:06:42 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const anonVariable = "";
+</script>
+
+<template>
+  <div class="flex items-center justify-center">
+    <p class="text-lg">html here</p>
+  </div>
+</template>
+
+<style>
+.your-css-class {
+}
+</style>
