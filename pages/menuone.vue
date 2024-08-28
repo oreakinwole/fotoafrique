420de72ac4436f86a066d19ef4b284e6937fdc0f
oreakinwole <oreakinwole@gmail.com>
2024-08-28 13:31:55 +0100

    and menu page

---
+++ pages/menuone.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const anonVariable = "";
+</script>
+
+<template>
+  <div>
+    <div class="">
+    <p class="font-['Playfair-Display'] text-6xl py-10 font-[600]">Custom<span class="italic text-[#A89C93]"> 3D Design</span> Assets</p>
+
+    </div>
+  
+  </div>
+</template>
+
