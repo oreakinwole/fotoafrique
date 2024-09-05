3286827437148c3ff52e87bda6741d0f4d70c44e
oreakinwole <oreakinwole@gmail.com>
2024-09-05 14:46:56 +0100

    fix loader

--- app.vue
+++ app.vue
@@ -3,13 +3,13 @@
     <NuxtLoadingIndicator
       color="black"
       :duration="2000"
-      :height="700"
+      :height="1200"
       :throttle="0"
     >
       <template #default>
-        <div class="h-full flex justify-center items-center">
+        <div class="h-full">
           <div
-            class="w-48 h-48 rounded-full border border-loader-brown text-center pt-[4em]"
+            class="w-48 h-48 rounded-full border border-loader-brown text-center pt-[4em] absolute left-[44%] top-[21%]"
           >
             <p class="text-loader-brown text-2xl">95%</p>
             <p class="text-xs text-loader-brown">Loading...</p>
