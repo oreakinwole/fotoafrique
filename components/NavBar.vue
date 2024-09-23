91f901674766fe73093b544a952937b0366616ed
oreakinwole <oreakinwole@gmail.com>
2024-09-23 09:17:38 +0100

    edit navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -92,7 +92,7 @@ export default {
 
     <!-- second layout  -->
     <div class="flex flex-row mt-10 justify-between text-xs">
-      <div class="flex flex-row gap-14 uppercase ml-36">
+      <div class="invisible flex-row gap-14 uppercase ml-36">
         <p class="cursor-pointer ">About</p>
         <div class="flex gap-2 cursor-pointer">
           <div>
@@ -193,50 +193,7 @@ export default {
 
 
 
-        <!-- More select button -->
-        <div class="relative inline-block text-left" @click="toggleMoreDropdown">
-          <!-- Selected Option -->
-          <button class="bg-navbar-color uppercase cursor-pointer inline-flex items-center justify-between w-full p-2"
-                  :class="{'underline-black': activeDropdown === 'more'}">
-            <span class="text-xs">More</span>
-            <svg :class="['w-5 h-5 ml-2 -mr-1', getArrowDirection(isMoreOpen)]" fill="currentColor" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 011.06 1.06l-4.25 4.25a.75.75 0 01-1.06 0L5.23 8.29a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-            </svg>
-          </button>
-
-          <div v-if="isMoreOpen" class="absolute left-[-61rem] z-10 mt-[1px] w-[93rem] origin-top-right bg-navbar-color border border-black border-t-0 border-opacity-20 rounded-bl-[25px] rounded-br-[25px] shadow-custom-light space-y-4">
-            <!-- Dropdown content -->
-            <div class="py-1 flex flex-row gap-10 px-10 mt-5 mb-5">
-              <!-- first container -->
-              <button @click="selectOption('More')" class="block w-full text-left px-4 py-2 border border-solid border-[#E4DBD1] h-[251px] rounded-[25px]">
-                <div class="ml-3 mt-2">
-                  <p class="font-[600] text-[16px] text-black">Financing</p>
-                  <p class="font-[400] text-[14px] text-[#A89C93] mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div class="w-[300px] h-[154px] mt-7 ml-20 bg-[#A89C93] rounded-tl-[20px] rounded-tr-[20px] "></div>
-              </button>
-
-              <!-- second container -->
-              <button @click="selectOption('More')" class="block w-full text-left px-4 py-2 border border-solid border-[#E4DBD1] h-[251px] rounded-[25px]">
-                <div class="ml-3 mt-2">
-                  <p class="font-[600] text-[16px] text-black">FAQs</p>
-                  <p class="font-[400] text-[14px] text-[#A89C93] mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div class="w-[300px] h-[154px] mt-7 ml-20 bg-[#665244] rounded-tl-[20px] rounded-tr-[20px] "></div>
-              </button>
-
-              <!-- third container -->
-              <button @click="selectOption('More')" class="block w-full text-left px-4 py-2 border border-solid border-[#E4DBD1] h-[251px] rounded-[25px]">
-                <div class="ml-3 mt-2">
-                  <p class="font-[600] text-[16px] text-black">Social</p>
-                  <p class="font-[400] text-[14px] text-[#A89C93] mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div class="w-[300px] h-[154px] mt-7 ml-20 bg-[#D0C2B0] rounded-tl-[20px] rounded-tr-[20px] "></div>
-              </button>
-            </div>
-          </div>
-        </div>
-
+       
       </div>
     </div>
   </div>
