15a8bba42545381eb4610c7aeb5d2ea75e91a8db
oreakinwole <oreakinwole@gmail.com>
2024-10-19 21:09:22 +0100

    why choose moderyardz

--- components/features/HomeThirdSegment/index.vue
+++ components/features/HomeThirdSegment/index.vue
@@ -41,40 +41,61 @@
 
   <div class="py-48 pb-96 bg-white">
     <h1
-      class="font-playfairDisplay text-center font-bold text-9xl text-[#665244] opacity-50"
+      class="font-playfairDisplay text-left pl-20 font-bold text-9xl text-[#665244] opacity-50"
       data-aos="slide-left"
       data-aos-duration="2000"
     >
       Why Choose
     </h1>
     <h1
-      class="font-playfairDisplay text-center font-bold text-9xl text-[#665244] opacity-50"
+      class="font-playfairDisplay text-right pr-20 font-bold text-9xl text-[#665244] opacity-50"
       data-aos="slide-right"
       data-aos-duration="2000"
     >
       Modern Yardz?
     </h1>
 
-    <div class="max-w-screen-sm relative ml-[25em] mt-28">
-      <p
-        class="font-inter text-[#665244] font-[400] text-xs max-w-xs leading-loose"
+    <div class="max-w-screen-lg relative pl-36 mx-auto mt-28 space-y-8">
+      <div
+        class="font-inter text-[#665244] max-w-md font-[400] text-xs leading-loose"
       >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel
-        dictum diam. Vivamus dignissim turpis nec dui molestie, sit amet
-        vestibulum tortor feugiat.
-      </p>
+        <span class="rounded-2xl font-semibold px-4 py-1 bg-[#F5F0EA]">01</span>
+        <p class="mt-4">
+          At Modern Yardz, our mission is simple: to transform your outdoor
+          space into something truly special. Based right here in San Diego,
+          we’ve spent decades perfecting the craft of landscape design. From
+          custom patios to complete backyard makeovers, we bring hands-on
+          experience and a personalized touch to every project.
+        </p>
+      </div>
+      <div
+        class="font-inter text-[#665244] max-w-md font-[400] text-xs leading-loose"
+      >
+        <span class="rounded-2xl font-semibold px-4 py-1 bg-[#F5F0EA]">02</span>
+        <p class="mt-4">
+          At Modern Yardz, our mission is simple: to transform your outdoor With
+          over 40 years in the business, we understand what it takes to create
+          outdoor spaces that are both beautiful and functional. Our local roots
+          mean we know San Diego’s unique landscape, and we’re committed to
+          delivering results that exceed your expectations.
+        </p>
+      </div>
 
-      <OutlinedButton
-        border-color="#000"
-        text-color="#000"
-        background-color="white"
-        text="SCHEDULE A CALL"
-        class="mt-10"
-      />
+      <div
+        class="font-inter text-[#665244] max-w-md font-[400] text-xs leading-loose"
+      >
+        <span class="rounded-2xl font-semibold px-4 py-1 bg-[#F5F0EA]">03</span>
+        <p class="mt-4">
+          Whether you’re looking to revamp your yard or start from scratch,
+          we’re here to guide you through every step. Trust us to turn your
+          vision into reality—because you deserve a space you’ll love for years
+          to come.
+        </p>
+      </div>
 
       <img
         class="absolute -top-40 right-0"
-        width="300"
+        width="400"
         src="@/assets/images/designerr__.png"
         data-aos="slide-up"
         data-aos-duration="2000"
