8f16eaccb37ae2a0f7675d6d3962039c18612c24
oreakinwole <oreakinwole@gmail.com>
2024-10-30 13:53:48 +0100

    edit hero mobile

--- components/HeroNew.vue
+++ components/HeroNew.vue
@@ -36,20 +36,20 @@ onMounted(() => {
 <template>
   <div>
     <div
-      class="min-h-screen pt-28 bg-[url('@/assets/images/hero_bg1.png')] bg-no-repeat bg-top bg-cover"
+      class="lg:min-h-screen pt-28 bg-[url('@/assets/images/hero_bg1.png')] bg-no-repeat bg-top bg-cover"
     >
       <div class="text-center space-y-3">
         <VueWriter
-          class="text-md font-semibold tracking-[.6em]"
+          class="text-xs lg:text-md font-semibold tracking-[.6em]"
           :array="[`DON'T JUST LIKE YOUR YARD`]"
           :iterations="1"
           :typeSpeed="100"
         />
-        <p class="text-8xl font-bold flex items-center justify-center">
+        <p class="text-6xl lg:text-8xl font-bold flex items-center justify-center">
           L
           <span
-            class="transition-all delay-[2600ms] duration-1000 ease-in-out w-9 h-9 py-6 px-7 border-[12px] border-black rounded-full hover:rounded-5xl"
-            :class="animateCaption && 'lg:px-36'"
+            class="transition-all delay-[2600ms] duration-1000 ease-in-out w-9 h-9 py-4 lg:py-6 px-4 lg:px-7 border-[9px] lg:border-[12px] border-black rounded-full hover:rounded-5xl"
+            :class="animateCaption && 'px-20 lg:px-36'"
           >
             &nbsp;
           </span>
@@ -60,7 +60,7 @@ onMounted(() => {
           >
         </p>
         <p
-          class="text-xs  max-w-xl mx-auto tracking-[.3em] leading-relaxed"
+          class="text-xs hidden lg:block max-w-lg  lg:max-w-xl mx-auto tracking-[.3em] leading-relaxed"
         >
           Let us turn your outdoor space into a beautiful, <br />
           custom-designed haven that you’ll enjoy for years to come.
@@ -69,7 +69,7 @@ onMounted(() => {
 
       <div
         id="hero-vid-sect"
-        class="w-4/5 h-[780px] bg-blue-100 mt-8 shadow-2xl rounded-2xl border-black border-8 mx-auto anihero"
+        class="w-4/5 lg:h-[780px] bg-blue-100 mt-8 shadow-2xl rounded-2xl border-black border-8 mx-auto anihero"
       >
         <video
           class="w-full h-full bg-blue-100 object-cover"
