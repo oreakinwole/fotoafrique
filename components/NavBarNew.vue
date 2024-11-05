89c0029763f458e36d90ff276a454eccf564724f
Divinegirl8 <iamoluchimercy@gmail.com>
2024-11-05 18:38:55 +0100

    implemented the landing page medium screen size responsiveness

--- components/NavBarNew.vue
+++ components/NavBarNew.vue
@@ -122,6 +122,7 @@ const handleBookConsult = () => {
                 @click="isShowMobileNav = false"
               >
                 <NuxtLink to="/services">Services</NuxtLink>
+                <NuxtLink to="/services">Services</NuxtLink>
               </li>
               <li
                 class="text-5xl lg:text-6xl hover:scale-x-90 duration-200 ease-linear font-semibold"
---
+++ pages/gallery.vue
@@ -0,0 +1,45 @@
+<script></script>
+
+<style></style>
+
+
+<template>
+    <div>
+      <div class="px-4 lg:px-20 lg:pt-28">
+        <!-- <p class="lg:hidden font-playfairDisplay text-4xl font-medium">
+          Portfoli<span class="text-new-gray font-imperial text-4xl">O</span>
+        </p> -->
+           <section>
+      
+            <p class="font-playfairDisplay text-4xl font-medium">
+              Paver 
+              <span class="text-new-gray font-imperial text-5xl">G</span>allery
+            </p>
+              
+  
+          </section>
+  
+          <section class="h-[420px] lg:h-[680px]">
+            <p class="font-playfairDisplay text-4xl font-medium">
+              Concrete
+              <span class="text-new-gray font-imperial text-5xl">G</span>allery
+            </p>
+              
+          </section>
+  
+          <section class="h-[510px] lg:h-[680px]">
+            <div>
+              <p class="font-playfairDisplay text-4xl font-medium">
+                Turf
+                <span class="text-new-gray font-imperial text-5xl">G</span>allery
+              </p> </div>
+  
+        
+          </section>
+        </div>
+      </div>
+  
+      <OurPortfolio />
+      <!-- <GetYourYard /> -->
+ 
+  </template>
