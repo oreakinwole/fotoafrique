0d3f4c3e5bc87d0935f44147e48ec4218b662539
Divinegirl8 <iamoluchimercy@gmail.com>
2024-11-06 18:05:59 +0100

    implememnted finance-page

---
+++ pages/finance.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="flex justify-between mx-auto mt-16">
+
+      
+        <div class="mt-[10rem] p-6 ml-20  max-w-[50%]">
+            <h1 class="uppercase font-playfairDisplay text-[3rem] font-semibold ">Partner with Our Trusted Finance Providers</h1>
+            <FilledButton
+                background-color="black"
+                text="Book Free Design Consultation"
+                text-color="white"
+                class="uppercase w-[90%] font-semibold lg:w-[290px] mt-10"
+            />
+        </div>
+
+ 
+        <div class="  mr-[13rem]">
+            <div class="flex justify-between mt-24 ">
+                <img src="@/assets/images/finance/image 215.png" class="w-[270px] h-[270px]" />
+                <img src="@/assets/images/finance/2.png" class="w-[270px] h-[270px]" />
+            </div>
+            <div class="flex justify-between">
+                <img src="@/assets/images/finance/3.png" class="w-[270px] h-[270px]" />
+                <img src="@/assets/images/finance/4.png" class="w-[270px] h-[270px]" />
+            </div>
+        </div>
+    </div>
+
+    <section class="text-center mx-4 lg:mx-0 mt-[15rem] mb-24">
+      <p class="text-sm lg:text-md lg:max-w-screen-md leading-8 mx-auto text-[#665244] mb-12">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel
+        dictum diam. Vivamus dignissim turpis nec dui molestie, sit amet
+        vestibulum tortor feugiat. Lorem ipsum dolor sit amet, consectetur
+        adipiscing. Suspendisse pharetra est nunc, eget varius libero pretium
+      </p>
+
+      <OutlinedButton
+        border-color="#000"
+        text-color="#000"
+        background-color="white"
+        text="SCHEDULE A CALL"
+        class="lg:mt-10"
+      />
+    </section>
+
+    <OurPortfolio class="" />
+</template>
