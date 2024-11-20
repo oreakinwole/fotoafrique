81cb53690659f64203449a423514f6a790ef81a8
oreakinwole <oreakinwole@gmail.com>
2024-11-20 16:19:03 +0100

    edit finance page

--- pages/finance.vue
+++ pages/finance.vue
@@ -1,46 +1,82 @@
 <template>
-    <div class="flex justify-between mx-auto mt-16">
-
-      
-        <div class="mt-[10rem] p-6 ml-20  max-w-[50%]">
-            <h1 class="uppercase font-playfairDisplay text-[3rem] font-semibold ">Partner with Our Trusted Finance Providers</h1>
-            <FilledButton
-                background-color="black"
-                text="Book Free Design Consultation"
-                text-color="white"
-                class="uppercase w-[90%] font-semibold lg:w-[290px] mt-10"
-            />
-        </div>
-
- 
-        <div class="  mr-[13rem]">
-            <div class="flex justify-between mt-24 ">
-                <img src="@/assets/images/finance/image 215.png" class="w-[270px] h-[270px]" />
-                <img src="@/assets/images/finance/2.png" class="w-[270px] h-[270px]" />
-            </div>
-            <div class="flex justify-between">
-                <img src="@/assets/images/finance/3.png" class="w-[270px] h-[270px]" />
-                <img src="@/assets/images/finance/4.png" class="w-[270px] h-[270px]" />
-            </div>
-        </div>
+  <div class="flex flex-col lg:flex-row  justify-between items-center lg:px-20">
+    <div class="p-6 flex-1">
+      <h1 class="uppercase font-playfairDisplay text-center lg:text-left text-[2rem] lg:text-[3rem] font-semibold">
+        Partner with Our Trusted Finance Providers
+      </h1>
+      <FilledButton
+        background-color="black"
+        text="Book Free Design Consultation"
+        text-color="white"
+        class="hidden lg:block uppercase w-[90%] font-semibold lg:w-[290px] mt-10"
+      />
     </div>
 
-    <section class="text-center mx-4 lg:mx-0 mt-[15rem] mb-24">
-      <p class="text-sm lg:text-md lg:max-w-screen-md leading-8 mx-auto text-[#665244] mb-12">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel
-        dictum diam. Vivamus dignissim turpis nec dui molestie, sit amet
-        vestibulum tortor feugiat. Lorem ipsum dolor sit amet, consectetur
-        adipiscing. Suspendisse pharetra est nunc, eget varius libero pretium
-      </p>
+    <div class="flex-1">
+      <div class="flex justify-between mt-8 lg:mt-20">
+        <div
+          class="flex-1 max-w-[700px] hover:scale-110 duration-200 ease-linear"
+        >
+          <NuxtLink
+            to="https://www.acornfinance.com/pre-qualify/?d=ABD05"
+            target="_blank"
+          >
+            <img
+              src="@/assets/images/finance/1.jpg"
+              alt="Image 2"
+              class="w-full h-auto"
+            />
+          </NuxtLink>
+        </div>
+        <div
+          class="flex-1 max-w-[700px] hover:scale-110 duration-200 ease-linear"
+        >
+          <NuxtLink
+            to="https://www.acornfinance.com/pre-qualify/?d=ABD05"
+            target="_blank"
+          >
+            <img
+              src="@/assets/images/finance/2.jpg"
+              alt="Image 2"
+              class="w-full h-auto"
+            />
+          </NuxtLink>
+        </div>
+      </div>
+      <div class="flex justify-between mt-8">
+        <div
+          class="flex-1 max-w-[700px] hover:scale-110 duration-200 ease-linear"
+        >
+          <NuxtLink
+            to="https://www.acornfinance.com/pre-qualify/?d=ABD05"
+            target="_blank"
+          >
+            <img src="@/assets/images/finance/3.jpg" class="w-full h-auto" />
+          </NuxtLink>
+        </div>
+        <div
+          class="flex-1 max-w-[700px] hover:scale-110 duration-200 ease-linear"
+        >
+          <NuxtLink
+            to="https://www.acornfinance.com/pre-qualify/?d=ABD05"
+            target="_blank"
+          >
+            <img src="@/assets/images/finance/4.jpg" class="w-full h-auto" />
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </div>
 
-      <OutlinedButton
-        border-color="#000"
-        text-color="#000"
-        background-color="white"
-        text="SCHEDULE A CALL"
-        class="lg:mt-10"
-      />
-    </section>
+  <section class="text-center mx-4 mt-20">
+    <OutlinedButton
+      border-color="#000"
+      text-color="#000"
+      background-color="white"
+      text="SCHEDULE A CALL"
+      class="lg:w-[230px] "
+    />
+  </section>
 
-    <!-- <FAQs class="" /> -->
+  <OurPortfolio class="" />
 </template>
