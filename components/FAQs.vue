ebe7eb632c6706315ba778358bc031c9bf922fb1
oreakinwole <oreakinwole@gmail.com>
2024-12-07 15:53:04 +0100

    edit faqs

--- components/FAQs.vue
+++ components/FAQs.vue
@@ -27,7 +27,7 @@ props.faqData?.dataArray &&
   <!--our portfolio -->
   <div
     id="FAQS"
-    class="w-full bg-black text-white pt-20 lg:pt-20 cursor-help"
+    class=" bg-white text-black pt-10 cursor-help"
     v-if="faqData.dataArray.length > 0"
   >
     <div class="space-y-4 lg:space-y-8 text-center pb-14 lg:pb-20">
@@ -45,33 +45,33 @@ props.faqData?.dataArray &&
     </div>
 
     <!-- FAQ Section -->
-    <div class="lg:px-5">
+    <div class="lg:px-5 space-y-8">
       <div
         v-for="(faq, index) in faqData.dataArray"
         :key="faq.question"
-        class="lg:max-w-[600px] border-t border-b border-[#F5F0EA] py-3 mx-3 lg:mx-auto relative"
+        class="lg:max-w-[600px] text-center border-t border-b border-[#F5F0EA] py-3 mx-3 lg:mx-auto relative"
         @mouseenter="faq.hover = true"
         @mouseleave="faq.hover = false"
         @click="isViewingFaq = index"
       >
-        <h4 class="font-[600]   text-lg tracking-widest flex-grow">
+        <h4 class="font-[600]   text-sm tracking-widest flex-grow">
           {{ faq.name }}
         </h4>
 
         <p
           v-if="isViewingFaq === index"
-          class="lg:text-md text-black font-light tracking-widest bg-white p-3 mt-12 animate__animated animate__slideInLeft"
+          class="lg:text-sm text-black font-light tracking-widest bg-white p-3 mt-12 animate__animated animate__slideInLeft"
         >
           {{ faq.acceptedAnswer.text }}
         </p>
       </div>
     </div>
 
-    <div class="flex justify-center mx-auto py-20 pb-32">
+    <div class="flex justify-center mx-auto py-20">
       <OutlinedButton(BLACK)
         border-color="#fff"
         text-color="#fff"
-        background-color="transparent"
+        background-color="black"
         text="GET STARTED TODAY!"
         class="lg:mt-10 lg:w-[200px]"
       />
